41b4e2cfbe1b1f1ae2becf89c14ae3391ba19762
johnmeshulam <johnmeshulam@gmail.com>
2021-02-03 22:14:05 +0200

    bump version

--- src/main/java/blockynoob/waterskin/common/main/WaterSkin.java
+++ src/main/java/blockynoob/waterskin/common/main/WaterSkin.java
@@ -16,7 +16,7 @@ public class WaterSkin
 {
     public static final String MODID = "waterskin";
     public static final String NAME = "Water Skin";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String MC_VERSION = "[1.12.2]";
 
     private static Logger logger;
